79273994086aea18a292bfc81f8d98565ecb981a
Alex Schokking <aschokking@gmail.com>
2025-12-20 23:05:52 -0800

    day 4 part 2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -5,7 +5,13 @@ advent_of_code::solution!(4);
 
 pub fn part_one(input: &str) -> Option<u64> {
     let map: Array2<char> = advent_of_code::parse_input(input);
-    let mut num_valid: usize = 0;
+    let valid_rolls = find_valid_rolls(&map).len();
+
+    Some(valid_rolls as u64)
+}
+
+fn find_valid_rolls(map: &Array2<char>) -> Vec<(usize, usize)> {
+    let mut valid_rolls: Vec<(usize, usize)> = Vec::new();
     // iterate over the index of each coordinate in the map array
     for (i, row) in map.axis_iter(ndarray::Axis(0)).enumerate() {
         for (j, value) in row.iter().enumerate() {
@@ -22,25 +28,40 @@ pub fn part_one(input: &str) -> Option<u64> {
                     continue;
                 }
 
-
                 if map[[ci as usize, cj as usize]] == '@' {
                     num_rolls += 1;
                 }
             }
 
             if num_rolls <= 3 {
-                num_valid += 1;
+                valid_rolls.push((i, j));
             }
 
             // dbg!(i, j, value, num_rolls, num_valid);
         }
     }
 
-    Some(num_valid as u64)
+    valid_rolls
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut map: Array2<char> = advent_of_code::parse_input(input);
+    let mut total = 0;
+    // loop until valid_rolls is empty
+    loop {
+        let valid_rolls = find_valid_rolls(&map);
+        if valid_rolls.is_empty() {
+            break;
+        }
+        total += valid_rolls.len() as u64;
+
+        // remove all valid rolls from the map
+        for (i, j) in valid_rolls {
+            map[[i, j]] = 'x';
+        }
+    }
+
+    Some(total)
 }
 
 #[cfg(test)]
@@ -56,6 +77,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(43));
     }
 }
