9ab2eeb4a8ee90a7d2bdb6a7847d38c53155479e
Alex Schokking <aschokking@gmail.com>
2025-12-20 22:58:17 -0800

    day 4 part 1

---
+++ src/bin/04.rs
@@ -0,0 +1,61 @@
+use advent_of_code::ALL_DIRECTIONS_8;
+use ndarray::Array2;
+
+advent_of_code::solution!(4);
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let map: Array2<char> = advent_of_code::parse_input(input);
+    let mut num_valid: usize = 0;
+    // iterate over the index of each coordinate in the map array
+    for (i, row) in map.axis_iter(ndarray::Axis(0)).enumerate() {
+        for (j, value) in row.iter().enumerate() {
+            if *value != '@' {
+                continue;
+            }
+
+            let mut num_rolls = 0;
+            // check the coordiantes around this one
+            for (di, dj) in ALL_DIRECTIONS_8.iter() {
+                let (ci, cj) = (i as i32 + di, j as i32 + dj);
+                if ci < 0 || ci >= map.shape()[0] as i32 || cj < 0 || cj >= map.shape()[1] as i32 {
+                    // skip this coordinate if it is out of bounds
+                    continue;
+                }
+
+
+                if map[[ci as usize, cj as usize]] == '@' {
+                    num_rolls += 1;
+                }
+            }
+
+            if num_rolls <= 3 {
+                num_valid += 1;
+            }
+
+            // dbg!(i, j, value, num_rolls, num_valid);
+        }
+    }
+
+    Some(num_valid as u64)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(13));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
